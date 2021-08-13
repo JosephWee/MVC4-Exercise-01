8763f40a8e334b74da4b4e795f6e975c9e1b474d
JosephWee <joseph.wee.tze.yeong@gmail.com>
2021-08-13 16:03:32 +0800

    Commit 2021-08-13-1601 Learning about CORS

---
+++ MVC4WebApp/Scripts/MyApps.js
@@ -0,0 +1,59 @@
+﻿class App1 {
+
+    constructor(rootElementId) {
+        $(rootElementId).html();
+
+        $.get("https://api.teleport.org/api/cities/",
+            function (data) {
+                if (data) {
+                    if (console)
+                        console.log(data);
+                }
+            })
+            .done(
+                function (data) {
+                    if (data) {
+                        if (console)
+                            console.log(data);
+                    }
+                })
+            .fail(
+                function (msg) {
+                    if (msg) {
+                        if (console)
+                            console.log(msg);
+                    }
+                }
+            );
+    }
+}
+
+class App2 {
+
+    constructor(rootElementId) {
+        $(rootElementId).html();
+
+        $.get("https://unsplash.com/photos/kelqkytHsPM",
+            function (data) {
+                if (data) {
+                    if (console)
+                        console.log(data);
+                }
+            })
+            .done(
+                function (data) {
+                    if (data) {
+                        if (console)
+                            console.log(data);
+                    }
+                })
+            .fail(
+                function (msg) {
+                    if (msg) {
+                        if (console)
+                            console.log(msg);
+                    }
+                }
+            );
+    }
+}
